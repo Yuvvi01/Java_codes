959a9a3b1ada9cbd07e4f54fe531acc4921274ee
Yuvvi01 <70095030+Yuvvi01@users.noreply.github.com>
2022-01-18 20:45:46 +0530

    Add files via upload

---
+++ divide.java
@@ -0,0 +1,28 @@
+/**
+ * divide
+ */
+//Method 1
+/*
+//Java program to divide two numbers and print on the screen.
+public class divide {
+
+    public static void main(String[] args) {
+        float x = 18.2f;
+        float y = 2.08f;
+        float z = x / y;     
+        System.out.println(z);
+    }
+}
+*/
+//Method 2
+
+public class divide {
+
+    public static void main(String[] args) {
+        byte x = 18;
+        byte y = 2;
+        int  z = x / y;     
+        System.out.println(z);
+        
+    }
+}
