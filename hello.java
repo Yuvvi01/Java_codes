30832e985f978845e42f283d1f1f4780379a0576
Yuvvi01 <70095030+Yuvvi01@users.noreply.github.com>
2022-01-18 20:37:50 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,11 @@
+/**
+ * hello
+ */
+//Hello World! program in Java
+public class hello {
+
+    public static void main(String[] args) {
+        String x = "Hello World!";
+        System.out.println(x);
+    }
+}
