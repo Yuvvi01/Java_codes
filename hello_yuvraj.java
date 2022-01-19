f8cc390af6402aaa9b9694f117f603ed0e4cdb1f
Yuvvi01 <70095030+Yuvvi01@users.noreply.github.com>
2022-01-19 17:25:09 +0530

    Add files via upload

---
+++ hello_yuvraj.java
@@ -0,0 +1,10 @@
+/**
+ * hello1
+ */
+//Java program to print 'Hello' on screen and then print your name on a separate line
+public class hello_yuvraj {
+
+    public static void main(String[] args) {
+        System.out.println("Hello \nYuvraj");
+    }
+}
