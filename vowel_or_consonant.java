d1ab9aa6e184f38b17e174f2ab3bcc197627ab31
Yuvvi01 <70095030+Yuvvi01@users.noreply.github.com>
2022-01-23 00:52:29 +0530

    Add files via upload

---
+++ vowel_or_consonant.java
@@ -0,0 +1,14 @@
+//Java program to check vowels and consonanats
+public class vowel_or_consonant {
+
+    public static void main(String[] args) {
+        char ch = 'i';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
+    }
+}
+       
