d133847663dd06e090e8bee94f8c1c0cbecad4f7
Yuvvi01 <70095030+Yuvvi01@users.noreply.github.com>
2022-01-27 23:21:11 +0530

    Add files via upload

---
+++ function1.java
@@ -0,0 +1,18 @@
+/**
+ * Java program to add two numbers using function
+ */
+/**
+ * function1
+ */
+public class function1 {
+    public static int add(int a,int b){
+        int c = a+b;
+        return c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(12, 13));
+       
+        
+    }
+}
