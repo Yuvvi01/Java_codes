dcdabf19b1b41f4121fc518967788e3c1f37f9e7
Yuvvi01 <70095030+Yuvvi01@users.noreply.github.com>
2022-01-27 23:22:46 +0530

    Add files via upload

---
+++ function4.java
@@ -0,0 +1,30 @@
+/**
+ * Java program to print the middle element of a string
+ */
+import java.util.Scanner;
+public class function4 {
+  public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a string: ");
+        String str = in.nextLine();
+        System.out.print("The middle character in the string: " + middle(str)+"\n");
+        in.close();
+    }
+ public static String middle(String str)
+    {
+        int position;
+        int length;
+        if (str.length() % 2 == 0)
+        {
+            position = str.length() / 2 - 1;
+            length = 2;
+        }
+        else
+        {
+            position = str.length() / 2;
+            length = 1;
+        }
+        return str.substring(position, position + length);
+    }
+}
